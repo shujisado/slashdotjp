073ce225f1d44ab546d7b0ed2bf2bbe7ce704e33
oliver <oliver@d264ba54-b42b-0410-8910-a1d6005a8e8b>
2005-01-04 10:29:17 +0000

    convert string to real UTF-8 before calculating MD5

--- slashjp/trunk/Slash/XML/XML.pm
+++ slashjp/trunk/Slash/XML/XML.pm
@@ -26,6 +26,7 @@ Slash::XML aids in creating XML.  Right now, only RSS is supported.
 use strict;
 use Apache::Constants ':http';
 use Digest::MD5 'md5_hex';
+use Encode;
 use Time::Local;
 use Slash;
 use Slash::Utility;
@@ -144,7 +145,7 @@ sub xmlDisplay {
 		http_send({
 			content_type	=> 'text/xml',
 			filename	=> $opt->{filename},
-			etag		=> md5_hex($temp),
+			etag		=> md5_hex(encode_utf8($temp)),
 			content		=> $content
 		});
 	}
