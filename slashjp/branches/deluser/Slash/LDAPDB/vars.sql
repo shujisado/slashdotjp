28daac7e3447ea3f0542b0083b31952388d44ef2
caesar <caesar@d264ba54-b42b-0410-8910-a1d6005a8e8b>
2007-10-16 11:10:46 +0000

    deluser impl

---
+++ slashjp/branches/deluser/Slash/LDAPDB/vars.sql
@@ -0,0 +1,7 @@
+INSERT IGNORE INTO vars (name, value, description) VALUES ('ldap_enable', '0', '');
+INSERT IGNORE INTO vars (name, value, description) VALUES ('ldap_server', 'localhost', '');
+INSERT IGNORE INTO vars (name, value, description) VALUES ('ldap_base_dn', '', '');
+INSERT IGNORE INTO vars (name, value, description) VALUES ('ldap_bind_dn', '', '');
+INSERT IGNORE INTO vars (name, value, description) VALUES ('ldap_bind_passwd', '', '');
+INSERT IGNORE INTO vars (name, value, description) VALUES ('ldap_attrib_prefix', '', '');
+INSERT IGNORE INTO vars (name, value, description) VALUES ('ldap_peer_attrib_prefix', '', '');
