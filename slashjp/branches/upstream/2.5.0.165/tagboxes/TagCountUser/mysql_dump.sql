ea7feb3fa9c4f200f93848da06d697eb572b576b
tach <tach@d264ba54-b42b-0410-8910-a1d6005a8e8b>
2007-10-18 14:07:12 +0000

    remove unnessesary upstream branch: 2.5.0.165

--- slashjp/branches/upstream/2.5.0.165/tagboxes/TagCountUser/mysql_dump.sql
+++
@@ -1,3 +0,0 @@
-# $Id: mysql_dump.sql,v 1.2 2006/05/30 18:37:54 jamiemccarthy Exp $
-INSERT INTO tagboxes (tbid, name, affected_type, weight, last_run_completed, last_tagid_logged, last_tdid_logged, last_tuid_logged) VALUES (NULL, 'TagCountUser', 'user', 1, '2000-01-01 00:00:00', 0, 0, 0);
-#INSERT INTO tagbox_userkeyregexes VALUES ('TagCountUser', '^tag_clout$');
