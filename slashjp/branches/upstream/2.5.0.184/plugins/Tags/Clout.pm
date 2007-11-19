c17afe01344670c0b5b14566b3231d22e01b5624
tach <tach@d264ba54-b42b-0410-8910-a1d6005a8e8b>
2007-11-19 07:21:26 +0000

    add upstream 2.5.0.184 branch

---
+++ slashjp/branches/upstream/2.5.0.184/plugins/Tags/Clout.pm
@@ -0,0 +1,68 @@
+# This code is a part of Slash, and is released under the GPL.
+# Copyright 1997-2005 by Open Source Technology Group. See README
+# and COPYING for more information, or see http://slashcode.com/.
+# $Id: Clout.pm,v 1.4 2007/10/24 21:12:23 jamiemccarthy Exp $
+
+package Slash::Clout;
+
+use strict;
+use warnings;
+use Slash;
+use Slash::Utility;
+use Slash::Tags;
+#use Slash::Clout::Describe;
+#use Slash::Clout::Vote;
+#use Slash::Clout::Moderate;
+
+use base 'Slash::DB::Utility';
+use base 'Slash::DB';
+
+use vars qw($VERSION);
+
+($VERSION) = ' $Revision: 1.4 $ ' =~ /\$Revision:\s+([^\s]+)/;
+
+sub init {
+	my($self) = @_;
+
+	$self->{months_back} = 4; # default
+	my $slashdb = getCurrentDB();
+        my $info = $slashdb->getCloutInfo();
+        for my $clid (keys %$info) {
+                $self->{clid} = $clid if $info->{$clid}{class} eq ref($self);
+        }
+        warn "cannot find clid for $self" if !$self->{clid};
+
+	my $constants = getCurrentStatic();
+	my $tags_reader = getObject('Slash::Tags', { db_type => 'reader' });
+	$self->{nodid} = $tags_reader->getTagnameidCreate($constants->{tags_upvote_tagname}   || 'nod');
+	$self->{nixid} = $tags_reader->getTagnameidCreate($constants->{tags_downvote_tagname} || 'nix');
+	1;
+}
+
+#sub insert_nextgen {
+#        my($g, $insert_ar) = @_;
+#        my $slashdb = getCurrentDB();
+#        for my $hr (@$insert_ar) {
+#                $hr->{gen} = $g; 
+#                $slashdb->sqlInsert('tags_peerweight', $hr);
+#        }
+#}       
+#        
+#sub update_tags_peerweight {
+#        my($insert_ar) = @_;
+#        for my $hr (@$insert_ar) {
+#                $tags_peerweight->{ $hr->{uid} } = $hr->{weight}; 
+#        } 
+#}               
+#                
+#sub B_copy_peerweight_sql {
+#        my $slashdb = getCurrentDB();
+#        $slashdb->sqlDo("SET AUTOCOMMIT=0");
+#        $slashdb->sqlDo("DELETE FROM users_param WHERE name='tagpeerval2'");
+#        $slashdb->sqlDo("INSERT INTO users_param SELECT NULL, uid, 'tagpeerval2', ROUND(weight,6)+0 FROM tags_peerweight");
+#        $slashdb->sqlDo("COMMIT");
+#        $slashdb->sqlDo("SET AUTOCOMMIT=1");
+#}
+
+1;
+
---
+++ slashjp/branches/upstream/2.5.0.184/themes/slashcode/tasks/rss_submit.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl -w
+# This code is a part of Slash, and is released under the GPL.
+# Copyright 1997-2005 by Open Source Technology Group. See README
+# and COPYING for more information, or see http://slashcode.com/.
+# $Id: rss_submit.pl,v 1.11 2005/05/04 18:48:09 pudge Exp $
+
+use strict;
+
+use File::Path;
+use Slash::Constants ':slashd';
+
+use vars qw( %task $me );
+
+my $total_freshens = 0;
+
+$task{$me}{timespec} = '1-59/3 * * * *';
+$task{$me}{timespec_panic_1} = '1-59/10 * * * *';
+$task{$me}{timespec_panic_2} = '';
+$task{$me}{on_startup} = 1;
+$task{$me}{fork} = SLASHD_NOWAIT;
+$task{$me}{code} = sub {
+	my($virtual_user, $constants, $slashdb, $user) = @_;
+	my %updates;
+	return 0
+		unless $constants->{rss_store};
+
+	my @time_long_ago = localtime(time-86400*$constants->{rss_expire_days});
+	my $weekago = sprintf "%4d-%02d-%02d", 
+		$time_long_ago[5] + 1900, $time_long_ago[4] + 1, $time_long_ago[3];
+
+	my $added_submissions = 0;
+	my $non_autosubmit = 0;
+	# First, we grab as many submissions as possible
+	my $rss_ar = $slashdb->getRSSNotProcessed($constants->{rss_process_number});
+	my $num_not_processed = $rss_ar ? scalar(@$rss_ar) : 0;
+	for my $rss (@$rss_ar) {
+		my $subid;
+		my $block = $slashdb->getBlock($rss->{bid});
+		my $description = $rss->{description} ? $rss->{description} : $rss->{title};
+		if ($block->{autosubmit} eq 'no') {
+			++$non_autosubmit;
+		} else {
+			my $blockskin = $slashdb->getSkin($block->{skin});
+			# createSubmission expects much of the data to be filtered
+			# already.  so deal!
+			my $submission = {
+				email	=> strip_attribute($rss->{link}),
+				name	=> strip_nohtml($block->{title}),
+				story	=> $description,
+				subj	=> strip_attribute($rss->{title}),
+				primaryskid => $blockskin->{skid},
+			};
+			$subid = $slashdb->createSubmission($submission);
+			if (!$subid) {
+				slashdLog("failed to createSubmission, rss title '$rss->{title}' from bid '$block->{bid}'");
+			} else {
+				$added_submissions++;
+			}
+		}
+		$slashdb->setRSS($rss->{id}, { processed => 'yes', subid => $subid });	
+	}
+
+	$slashdb->expireRSS($constants->{rss_process_number});
+
+	my $ret = "";
+	if ($added_submissions || $non_autosubmit) {
+		$ret = "totaladded Submissions $added_submissions; non-autosubmits $non_autosubmit; of $num_not_processed not processed";
+	}
+	return $ret;
+};
+
+1;
