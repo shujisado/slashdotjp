c80780f364d4f8e37e099e1d51689395dbf38456
tach <tach@d264ba54-b42b-0410-8910-a1d6005a8e8b>
2008-02-08 10:14:55 +0000

    [svn-buildpackage] Tagging slash (2.5.0.192-3)

---
+++ slashjp/tags/2.5.0.192-3/Slash/XML/FOAF/FOAF.pm
@@ -0,0 +1,191 @@
+# This code is a part of Slash, and is released under the GPL.
+# Copyright 1997-2003 by Open Source Development Network. See README
+# and COPYING for more information, or see http://slashcode.com/.
+# $Id$
+
+package Slash::XML::FOAF;
+
+=head1 NAME
+
+Slash::XML::FOAF - Perl extension for Slash
+
+
+=head1 SYNOPSIS
+
+	use Slash::XML;
+	xmlDisplay('foaf', %data);
+
+
+=head1 DESCRIPTION
+
+Module to output data about a user using the FOAF (Friend of a Friend)
+RDF-schema. See http://www.foaf-project.org/ for details about FOAF.
+
+
+=head1 EXPORTED FUNCTIONS
+
+=cut
+
+use strict;
+use Slash;
+use Slash::Utility;
+
+use base 'Slash::XML';
+use vars qw($VERSION);
+
+($VERSION) = ' $Revision$ ' =~ /\$Revision:\s+([^\s]+)/;
+
+my %foaf_elements = ( 
+	maker		=> "",
+	Agent		=> "",
+	Document	=> "",
+	Person		=> "",
+	mbox_sha1sum	=> "",
+	mbox		=> "resource",
+	nick		=> "",
+	weblog		=> "resource",
+	page		=> "resource",
+	homepage	=> "resource",
+	knows		=> "",
+	holdsAccount	=> "",
+	OnlineAccount	=> "",
+	accountName	=> "",
+	accountServiceHomepage => "resource",
+);
+
+#========================================================================
+
+=head2 create(PARAM)
+
+Creates FOAF.
+
+=over 4
+
+=item Parameters
+
+=over 4
+
+=item PARAM
+
+Hashref of parameters.  Currently supported options are below.
+
+=over 4
+
+=item Document 
+
+Hash containing various info about the generated FOAF document.
+Currently supported attributes are title (dc:title, human-readable
+Title of the document), date (dc:date, when the document
+was created/last-updated) and maker (foaf:maker, string describing the
+creator).
+
+=item Person
+
+Hash containing various info about the foaf:Person desribed in
+the generated FOAF document. Currently supported classes and
+properties from FOAF are: mbox_sha1sum, foaf:nick, weblog, page,
+homepage, knows, holdsAccount, OnlineAccount,accountName, 
+accountServiceHomepage and Person. Additionally, seeAlso
+can be specified and will be treated as belonging to the
+RDF Schema namespace.
+
+
+=back
+
+=back
+
+=item Return value
+
+The complete FOAF data as a string.
+
+=item NOTE
+
+The validity in regards to the FOAF schema is NOT checked. 
+The caller should make sure to abide by the rules.
+
+=back
+
+=cut
+
+
+sub create {
+	my($class, $param) = @_;
+	my $self = bless {}, $class;
+	my $ret;
+
+	# create bare foaf document
+	$ret =  "<?xml version='1.0'?>\n";
+	$ret .= "<rdf:RDF\n";
+	$ret .= "   xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n";
+	$ret .= "   xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"\n";
+	$ret .= "   xmlns:foaf=\"http://xmlns.com/foaf/0.1/\"\n";
+	$ret .= "   xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n";
+
+	# create info about current document
+	if (defined $param->{Document}){
+		my $document =  $param->{Document};
+		$ret .= "<foaf:Document rdf:about=''>\n";
+		if (defined $document->{title}){
+			$ret .= "  <dc:title>$document->{title}</dc:title>\n";
+		}
+		my $date = defined $document->{date}
+			? $document->{date}
+			: $self->date2iso8601();
+		my $maker = defined $document->{maker}
+			? $document->{maker}
+			: "Slashcode FOAF.pm $VERSION";
+
+		$ret .= "  <dc:date>$date</dc:date>\n";
+		$ret .= "  <foaf:maker><foaf:Agent>\n";
+		$ret .= "    <foaf:name>$maker</foaf:name>\n";
+		$ret .= "  </foaf:Agent></foaf:maker>\n";
+		$ret .= "</foaf:Document>\n";
+	}
+
+	#info about the person to be described
+	if (defined $param->{Person}){
+		$ret .= $self->_processFoaf( "Person", $param->{Person} ); 
+	}
+	$ret .= "</rdf:RDF>\n";
+
+	return $ret;
+}
+
+1;
+
+sub _processFoaf{
+	my ($self, $name, $node) = @_;
+	my $ret;
+	if (ref($node) eq "HASH" ){
+		$ret .= "<foaf:$name>\n";
+		foreach (keys %$node){
+			$ret .= $self->_processFoaf( $_, $node->{$_} );
+		}
+		$ret .= "</foaf:$name>\n";
+	}elsif (ref($node) eq "ARRAY"){
+		foreach (@$node){
+			$ret .= $self->_processFoaf( $name, $_ );
+		}
+        }else{
+		if ($foaf_elements{$name} eq "resource"){
+			$ret .= "<foaf:$name rdf:resource=\"$node\" />\n";
+		}elsif ($name eq "seeAlso"){
+			$ret .= "<rdfs:seeAlso rdf:resource=\"$node\" />\n";
+		}else{
+			$ret .= "<foaf:$name>$node</foaf:$name>\n";
+		}
+	}
+
+	return $ret;
+}
+
+__END__
+
+
+=head1 SEE ALSO
+
+Slash(3), Slash::XML(3).
+
+=head1 VERSION
+
+$Id$
---
+++ slashjp/tags/2.5.0.192-3/themes/slashcode/tasks/open_backend.pl
@@ -0,0 +1,238 @@
+#!/usr/bin/perl -w
+# This code is a part of Slash, and is released under the GPL.
+# Copyright 1997-2005 by Open Source Technology Group. See README
+# and COPYING for more information, or see http://slashcode.com/.
+# $Id$
+
+use strict;
+use Slash;
+use Slash::XML;
+use Slash::Constants ':slashd';
+
+use vars qw( %task $me %redirects );
+
+$task{$me}{timespec} = '0-59/10 * * * *';
+$task{$me}{timespec_panic_1} = ''; # not that important
+$task{$me}{fork} = SLASHD_NOWAIT;
+$task{$me}{code} = sub {
+	my($virtual_user, $constants, $slashdb, $user, $info, $gSkin) = @_;
+
+	load_redirects();
+
+	my $reader = getObject('Slash::DB', { db_type => 'reader' });
+	my $max_items = $constants->{rss_max_items_outgoing} || 10;
+
+	my $stories = $reader->getBackendStories({ limit => $max_items });
+	if ($stories && @$stories) {
+		newxml(@_, undef, $stories);
+		newrdf(@_, undef, $stories);
+		newrss(@_, undef, $stories);
+		newatom(@_, undef, $stories);
+	}
+
+	my $skins = $slashdb->getSkins();
+	for my $skid (keys %$skins) {
+		my $name = $skins->{$skid}{name};
+		my $nexus = $skins->{$skid}{nexus};
+		$stories = $reader->getBackendStories({ limit => $max_items, topic => $nexus });
+		if ($stories && @$stories) {
+			newxml(@_, $name, $stories);
+			newrdf(@_, $name, $stories);
+			newrss(@_, $name, $stories);
+			newatom(@_, $name, $stories);
+		}
+	}
+
+	return;
+};
+
+# this normalizes old and new content, stripping data that
+# updates every time
+sub fudge {
+	my($current, $new) = @_;
+	s{[dD]ate>[^<]+</}{} for $current, $new;
+	s{<(?:slash:)?(?:comments|hitparade)>[^<]+</}{}g for $current, $new;
+	return($current, $new);
+}
+
+sub _do_rss {
+	my($virtual_user, $constants, $slashdb, $user, $info, $gSkin,
+		$name, $stories, $version, $type) = @_;
+
+	$type ||= 'rss';
+
+	my $file    = sitename2filename($name);
+	my $skin    = { };
+	$skin       = $slashdb->getSkin($name) if $name;
+	my $link    = ($skin->{url}  || $gSkin->{absolutedir}) . '/';
+	my $title   = $constants->{sitename};
+	$title = "$title: $skin->{title}"
+		if $skin->{skid} && $skin->{skid} != $constants->{mainpage_skid} && $skin->{title};
+
+	my $description = $constants->{slogan};
+
+	# XXX: temporary, until we can add a skin_param table
+	if ($constants->{sitename} eq 'Slashdot') {
+		if ($skin->{title} && $skin->{title} eq 'AMD') {
+			$title .= ' Sponsored Content';
+			$description = 'Special Advertising Section';
+		}
+	}
+
+	my $ext = $version == 0.91 && $type eq 'rss' ? 'rdf' : $type;
+	my $filename = "$file.$ext";
+
+	if (-d "$constants->{basedir}/privaterss/") {
+		my $rss = xmlDisplay($type, {
+			channel		=> {
+				title		=> $title,
+				'link'		=> $link,
+				selflink	=> "$link$filename",
+				description	=> $description,
+			},
+			version		=> $version,
+			textinput	=> 1,
+			image		=> 1,
+			items		=> [ map { { story => $_ } } @$stories ],
+		}, 1);
+		save2file("$constants->{basedir}/privaterss/$filename", $rss, \&fudge);
+	}
+
+	my @items = map { { story => $_ } } @$stories;
+	if ($constants->{rss_no_public_static}) {
+		my $newurl = $redirects{$filename} || "(none for $filename)";
+		unshift @items, {
+			introtext	=> sprintf('This URL is no longer valid.  Please update your bookmarks to the new URL: %s', $newurl),
+			title		=> 'Please Update Your Bookmarks',
+			'link'		=> $newurl,
+			'time'		=> '2006-12-13 00:00:00',
+		};
+	}
+
+	my $rss = xmlDisplay($type, {
+		channel		=> {
+			title		=> $title,
+			'link'		=> $link,
+			selflink	=> "$link$filename",
+			description	=> $description,
+		},
+		version		=> $version,
+		textinput	=> 1,
+		image		=> 1,
+		items		=> \@items,
+	}, 1);
+
+	save2file("$constants->{basedir}/$filename", $rss, \&fudge);
+}
+
+sub newrdf  { _do_rss(@_, '0.91') } # RSS 0.91
+sub newrss  { _do_rss(@_, '1.0') } # RSS 1.0
+sub newatom { _do_rss(@_, '1.0', 'atom') } # Atom 1.0
+
+sub newxml {
+	my($virtual_user, $constants, $slashdb, $user, $info, $gSkin,
+		$name, $stories) = @_;
+
+	my $x = <<EOT;
+<?xml version="1.0"?><backslash
+xmlns:backslash="$gSkin->{absolutedir}/backslash.dtd">
+
+EOT
+
+	for my $story (@$stories) {
+		my @str = (xmlencode($story->{title}), xmlencode($story->{dept}));
+		my $author = $slashdb->getAuthor($story->{uid}, 'nickname');
+		$x.= <<EOT;
+	<story>
+		<title>$str[0]</title>
+		<url>$gSkin->{absolutedir}/article.pl?sid=$story->{sid}</url>
+		<time>$story->{'time'}</time>
+		<author>$author</author>
+		<department>$str[1]</department>
+		<topic>$story->{tid}</topic>
+		<comments>$story->{commentcount}</comments>
+		<section>$story->{section}</section>
+		<image>$story->{image}{image}</image>
+	</story>
+
+EOT
+	}
+
+	$x .= "</backslash>\n";
+
+	my $file = sitename2filename($name);
+	save2file("$constants->{basedir}/$file.xml", $x, \&fudge);
+}
+
+sub load_redirects {
+%redirects = (
+	'apache.rss'        => 'http://rss.slashdot.org/Slashdot/slashdotApache',
+	'apple.rss'         => 'http://rss.slashdot.org/Slashdot/slashdotApple',
+	'askslashdot.rss'   => 'http://rss.slashdot.org/Slashdot/slashdotAskSlashdot',
+	'awards.rss'        => 'http://rss.slashdot.org/Slashdot/slashdotThe2000Beanies',
+	'books.rss'         => 'http://rss.slashdot.org/Slashdot/slashdotBookReviews',
+	'bsd.rss'           => 'http://rss.slashdot.org/Slashdot/slashdotBsd',
+	'developers.rss'    => 'http://rss.slashdot.org/Slashdot/slashdotDevelopers',
+	'features.rss'      => 'http://rss.slashdot.org/Slashdot/slashdotFeatures',
+	'games.rss'         => 'http://rss.slashdot.org/Slashdot/slashdotGames',
+	'hardware.rss'      => 'http://rss.slashdot.org/Slashdot/slashdotHardware',
+	'index.rss'         => 'http://rss.slashdot.org/Slashdot/slashdot',
+	'interviews.rss'    => 'http://rss.slashdot.org/Slashdot/slashdotInterviews',
+	'it.rss'            => 'http://rss.slashdot.org/Slashdot/slashdotIt',
+	'linux.rss'         => 'http://rss.slashdot.org/Slashdot/slashdotLinux',
+	'politics.rss'      => 'http://rss.slashdot.org/Slashdot/slashdotPolitics',
+	'radio.rss'         => 'http://rss.slashdot.org/Slashdot/slashdotGeeksInSpace',
+	'science.rss'       => 'http://rss.slashdot.org/Slashdot/slashdotScience',
+	'search.rss'        => 'http://rss.slashdot.org/Slashdot/slashdotSearch',
+	'slashdot.rss'      => 'http://rss.slashdot.org/Slashdot/slashdot',
+	'tacohell.rss'      => 'http://rss.slashdot.org/Slashdot/slashdotTacoHell',
+	'yro.rss'           => 'http://rss.slashdot.org/Slashdot/slashdotYourRightsOnline',
+
+	'apache.rdf'        => 'http://rss.slashdot.org/Slashdot/slashdotApache/to',
+	'apple.rdf'         => 'http://rss.slashdot.org/Slashdot/slashdotApple/to',
+	'askslashdot.rdf'   => 'http://rss.slashdot.org/Slashdot/slashdotAskSlashdot/to',
+	'awards.rdf'        => 'http://rss.slashdot.org/Slashdot/slashdotThe2000Beanies/to',
+	'books.rdf'         => 'http://rss.slashdot.org/Slashdot/slashdotBookReviews/to',
+	'bsd.rdf'           => 'http://rss.slashdot.org/Slashdot/slashdotBsd/to',
+	'developers.rdf'    => 'http://rss.slashdot.org/Slashdot/slashdotDevelopers/to',
+	'features.rdf'      => 'http://rss.slashdot.org/Slashdot/slashdotFeatures/to',
+	'games.rdf'         => 'http://rss.slashdot.org/Slashdot/slashdotGames/to',
+	'hardware.rdf'      => 'http://rss.slashdot.org/Slashdot/slashdotHardware/to',
+	'index.rdf'         => 'http://rss.slashdot.org/Slashdot/slashdot/to',
+	'interviews.rdf'    => 'http://rss.slashdot.org/Slashdot/slashdotInterviews/to',
+	'it.rdf'            => 'http://rss.slashdot.org/Slashdot/slashdotIt/to',
+	'linux.rdf'         => 'http://rss.slashdot.org/Slashdot/slashdotLinux/to',
+	'politics.rdf'      => 'http://rss.slashdot.org/Slashdot/slashdotPolitics/to',
+	'radio.rdf'         => 'http://rss.slashdot.org/Slashdot/slashdotGeeksInSpace/to',
+	'science.rdf'       => 'http://rss.slashdot.org/Slashdot/slashdotScience/to',
+	'search.rdf'        => 'http://rss.slashdot.org/Slashdot/slashdotSearch/to',
+	'slashdot.rdf'      => 'http://rss.slashdot.org/Slashdot/slashdot/to',
+	'tacohell.rdf'      => 'http://rss.slashdot.org/Slashdot/slashdotTacoHell/to',
+	'yro.rdf'           => 'http://rss.slashdot.org/Slashdot/slashdotYourRightsOnline/to',
+
+	'apache.atom'       => 'http://rss.slashdot.org/Slashdot/slashdotApacheatom',
+	'apple.atom'        => 'http://rss.slashdot.org/Slashdot/slashdotAppleatom',
+	'askslashdot.atom'  => 'http://rss.slashdot.org/Slashdot/slashdotAskslashdotatom',
+	'awards.atom'       => 'http://rss.slashdot.org/Slashdot/slashdotAwardsatom',
+	'books.atom'        => 'http://rss.slashdot.org/Slashdot/slashdotBooksatom',
+	'bsd.atom'          => 'http://rss.slashdot.org/Slashdot/slashdotBsdatom',
+	'developers.atom'   => 'http://rss.slashdot.org/Slashdot/slashdotDevelopersatom',
+	'features.atom'     => 'http://rss.slashdot.org/Slashdot/slashdotFeaturesatom',
+	'games.atom'        => 'http://rss.slashdot.org/Slashdot/slashdotGamesatom',
+	'hardware.atom'     => 'http://rss.slashdot.org/Slashdot/slashdotHardwareatom',
+	'index.atom'        => 'http://rss.slashdot.org/Slashdot/slashdotatom',
+	'interviews.atom'   => 'http://rss.slashdot.org/Slashdot/slashdotInterviewsatom',
+	'it.atom'           => 'http://rss.slashdot.org/Slashdot/slashdotItatom',
+	'linux.atom'        => 'http://rss.slashdot.org/Slashdot/slashdotLinuxatom',
+	'politics.atom'     => 'http://rss.slashdot.org/Slashdot/slashdotPoliticsatom',
+	'radio.atom'        => 'http://rss.slashdot.org/Slashdot/slashdotRadioatom',
+	'science.atom'      => 'http://rss.slashdot.org/Slashdot/slashdotScienceatom',
+	'search.atom'       => 'http://rss.slashdot.org/Slashdot/slashdotSearchatom',
+	'slashdot.atom'     => 'http://rss.slashdot.org/Slashdot/slashdotatom',
+	'tacohell.atom'     => 'http://rss.slashdot.org/Slashdot/slashdotTacohellatom',
+	'yro.atom'          => 'http://rss.slashdot.org/Slashdot/slashdotYroatom',
+);
+}
+
+1;
+
