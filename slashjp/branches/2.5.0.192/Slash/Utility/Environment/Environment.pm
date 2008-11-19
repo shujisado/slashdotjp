0b97e54e6345cc97e284d7f2da4a4ff1601bf1f2
tach <tach@d264ba54-b42b-0410-8910-a1d6005a8e8b>
2008-11-19 08:47:27 +0000

    Fix for invalid form input at Slash/Utility/Environment/Environment.pm

--- slashjp/branches/2.5.0.192/Slash/Utility/Environment/Environment.pm
+++ slashjp/branches/2.5.0.192/Slash/Utility/Environment/Environment.pm
@@ -1988,7 +1988,7 @@ sub filter_param {
 		$data =~ s|[^a-zA-Z0-9_]+||g;
 	} else {
 		for my $ri (@regints) {
-			$data = fixint($data) if $ri =~ $key;
+			$data = fixint($data) if $ri =~ quotemeta($key);
 		}
 
 		# convert input to internal character encoding
