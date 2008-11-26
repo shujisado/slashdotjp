8a97f86fef7d51a576b4dd22b9ac73c949c44f45
tach <tach@d264ba54-b42b-0410-8910-a1d6005a8e8b>
2008-11-26 09:58:26 +0000

    copy to test images

---
+++ test/images/slashdot-it.js
@@ -0,0 +1,16 @@
+(
+  function() {
+    var url = encodeURIComponent(window.slashdot_url || window.location.href);
+    var sty = window.slashdot_badge_style || 'h0';
+    var title = encodeURIComponent(window.slashdot_title || document.title);
+    var dx=130, dy=25;
+    if ( sty[0]=='v' ) {
+      dx=52;
+      dy=80;
+    }
+    
+    var iframe = '<iframe src="http://slashdot.jp/slashdot-it.pl?style=' + sty + '&amp;url=' + url + '&amp;title=' + title + '"' +
+                  ' height="' + dy + '" width="' + dx + '" scrolling="no" frameborder="0"></iframe>'
+    document.write(iframe);
+  }
+)()
